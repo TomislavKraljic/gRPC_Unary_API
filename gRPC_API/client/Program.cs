3d5bec93443c4197430b39d8a62c5a9f1dcf7a47
Tomislav Kraljic <Tomislav Kraljic@DESKTOP-3L25HL7>
2021-02-05 23:40:48 -0800

    Add project files.

---
+++ gRPC_API/client/Program.cs
@@ -0,0 +1,44 @@
+﻿using Dummy;
+using Greet;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+
+            var client = new GreetingService.GreetingServiceClient(channel);
+
+            var greeting = new Greeting()
+            {
+                FirstName = "Tomislav",
+                LastName = "Kraljic"
+            };
+
+            var request = new GreetingRequest() { Greeting = greeting };
+
+            var response = client.Greet(request);
+
+            Console.WriteLine(response.Result);
+
+            channel.ShutdownAsync().Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
